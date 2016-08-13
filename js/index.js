3c628286a32931e851dd7c5c07cf78c2ca33abce
Ewa Manek <manekej@gmail.com>
2016-08-13 10:30:27 -0700

    Remove unnecessary files and add smooth scrolling on navbar click

--- js/index.js
+++ js/index.js
@@ -2,4 +2,17 @@ $(document).ready(function(){
   $("#myName").on('animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd', function(e) {
     $("#mySkillz").removeClass("light-text").addClass("fadeIn");
   });
+
+  $("nav a").on('click', function(event){
+  // Prevent default anchor click behavior
+    event.preventDefault();
+    // Store hash (#)
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 500, function(){
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+  });
 });
